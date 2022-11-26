27777edbce7233a8788b33fb83ea832eeb6e1078
keithkiama <keithkiama2003@gmail.com>
2022-11-26 15:57:49 +0300

    Updated

---
+++ toyproblems/studentgradegenerator.js
@@ -0,0 +1,27 @@
+//This is a calculator that outputs the grade of a student according to the percentage
+const maximumGrade = 100;
+function gradeCalculator(){
+    //Input the grade
+    let grade = 0;
+    const studentGrade = (grade / maximumGrade) * 100
+    if(studentGrade === 79){
+        return (`A`)
+    }
+
+    else if(studentGrade < 79 && studentGrade >= 60){
+        return(`B-`)
+    }
+    
+    else if(studentGrade <= 59 && studentGrade >= 49){
+        return(`C-`)
+    }
+
+    else if(studentGrade <= 49 && studentGrade >= 40){
+        return(`D-`)
+    }
+
+    else if(studentGrade > 40){
+        return(`E-`)
+    }
+}
+console.log(gradeCalculator())
